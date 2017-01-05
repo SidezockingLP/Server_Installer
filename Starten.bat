9f31cf17d66ac4c8748e9c063efbf0566ca726be
SidezockingLP <SidezockingLP@gmail.com>
2017-01-05 22:41:14 +0100

    Starten.bat
    Starten.bat

---
+++ Starten.bat
@@ -0,0 +1,26 @@
+@echo off
+Echo       ______ _     _                        _     _             _       ______  
+Echo      / _____I_I   l l                      l l   (_)           (_)     (_____ \ 
+Echo     ( (____  _  __l l_____ _____ ___   ____l l  _ _ ____   ____ _       _____) )
+Echo      \____ \l l/ _  l ___ (___  ) _ \ / ___I l_/ ) l  _ \ / _  l l     l  ____/ 
+Echo      _____) ) l (_l l ____l/ __/ l_l ( (___l  _ (l l l l ( (_l l l_____l l      
+Echo     (______/l_l\____l_____l_____)___/ \____)_l \_)_l_l l_l\___ l_______)_l      
+Echo                                                          (_____l   
+Echo.
+Echo.   
+Echo   ______                                        _______  
+Echo  / _____)                                      (_______) 
+Echo ( (____  _____  ____ _   _ _____  ____          _  _  _ _____ ____  _____  ____ _____  ____ 
+Echo  \____ \l ___ l/ ___) l l l ___ l/ ___)        l ll_ll (____ l  _ \(____ l/ _  l ___ l/ ___)
+Echo  _____) I ____l l    \ V /l ____l l            l l   l / ___ l l l / ___ ( (_l l ____l l   
+Echo (______/l_____l_l     \_/ l_____)_l            l_l   l_\_____l_l l_\_____l\___ l_____)_l 
+Echo                                                                          l_____l 
+Echo.
+Echo.
+Echo.
+Echo.
+Echo.
+Echo Starte Server!....
+timeout /t 3
+Echo. Server startet! Bitte einen Moment Gedult!
+
